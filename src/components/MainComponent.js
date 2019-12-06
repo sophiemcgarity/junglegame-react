8946ac67af2d8ae55ea70bcd368fa3730854df71
Sophie McGarity <sophiemcgarity7@gmail.com>
2019-12-06 11:35:04 -0800

    First commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Jungle Game</h1>
+            </div>
+        );
+    }
+}
+
+export default Main;
