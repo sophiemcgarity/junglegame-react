f69d72aac70147e88469bccce221888847824fb2
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-01-18 11:28:40 -0800

    story change

--- src/components/GamePageComponent.js
+++ src/components/GamePageComponent.js
@@ -6,36 +6,56 @@ class GamePage extends Component {
         super(props);
 
         this.state = {
-            selectedPage: 'start',
-        }
+            selectedPage: "river",
+        };
+
+        this.changePage = this.changePage.bind(this);
+        this.changePageBtn1 = this.changePageBtn1.bind(this);
+        this.changePageBtn2 = this.changePageBtn2.bind(this);
     }
 
     
     changePage() {
-        this.setState({ selectedPage: this.state.pages.id })
+        this.setState(state => ({ 
+            selectedPage: state.pages.id
+        }));
+    }
+
+    changePageBtn1 () {
+        this.setState({
+            selectedPage: this.state.pages.button1Id
+        });
+    }
+
+    changePageBtn2(){
+        this.setState({
+            selectedPage: this.state.pages.button2Id
+        });
     }
 
     render() {
         
         const currentPage = this.props.pages.filter(val => val.id === this.state.selectedPage);
 
+        //use button1Id and button2Id so when clicked takes the user to the correct next page
+        //use button1Id or button2Id to find the next page to render
+
         const button = currentPage.map(btn => {
             return (
                 <div key={btn.id}>
-                    <Button onClick={this.changePage}>{btn.button1}</Button>
-                    <Button onClick={this.changePage}>{btn.button2}</Button>
+                    <Button key={this.props.pages.button1Id} onClick={this.changePageBtn1}>{btn.button1}</Button>
+                    <Button key={this.props.pages.button2Id} onClick={this.changePageBtn2}>{btn.button2}</Button>
                 </div>
             )
         })
 
-        
-        const renderText = currentPage.map(i => i.story);
+        const renderStory = currentPage.map(i => i.story);
 
         const story = currentPage.map(s => {
             return (
                 <div key={s.id}>
                     <div className="storyText">
-                        {renderText}
+                        {renderStory}
                     </div>
                 </div>
             )
